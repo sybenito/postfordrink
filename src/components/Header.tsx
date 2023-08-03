cd5a9df1324d46f45dacf963f8f3c9156c89f8c6
Ben Sy <ben.sy@ttt.studio>
2023-08-03 09:16:54 -0700

    Add file upload component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const Header: FC = () => {
   return (
     <header className="app-header">
       <div className="logo">
-        <h1>Post n Drink</h1>
+        <h1>Post for Drinks</h1>
         {authContext.isSignedIn && <span className="user-name">&#8212;&nbsp; {authContext.user?.displayName}</span>}
       </div>
       <div className="login-action">{isSignedIn === true && <SignOut />}</div>
---
+++ src/components/PhotoUpload.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import type { FirebaseStorage } from "firebase/storage";
+import type { UploadRequestOption } from "rc-upload/lib/interface";
+import { CameraOutlined } from "@ant-design/icons";
+import { Upload, message } from "antd";
+import ImgCrop from "antd-img-crop";
+import useAuth from "../hooks/Auth";
+
+const storage: FirebaseStorage = getStorage();
+
+const PhotoUpload = () => {
+  const { fb } = useAuth();
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+
+  const handleUpload = (file: UploadRequestOption) => {
+    const storageRef = ref(storage, "photos/photo.jpg");
+    setIsUploading(true);
+    uploadBytesResumable(storageRef, file.file as Blob)
+      .then(() => {
+        message.success("Photo uploaded!");
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setIsUploading(false);
+      });
+  };
+
+  return (
+    <div className="main">
+      {fb && (
+        <div className="photo-upload">
+          <ImgCrop>
+            <Upload
+              name="photo"
+              listType="picture-card"
+              className="photo-uploader"
+              showUploadList={false}
+              accept="image/*"
+              capture="environment"
+              customRequest={handleUpload}
+              maxCount={1}
+              disabled={isUploading}
+            >
+              {!isUploading && <CameraOutlined />}
+              {isUploading && <CameraOutlined spin />}
+            </Upload>
+          </ImgCrop>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PhotoUpload;
