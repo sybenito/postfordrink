6b468adc231687b124e9445e0189c8f658331c09
Ben Sy <sy.benito@gmail.com>
2023-08-06 14:06:16 -0700

    confirmation modal for signout action

--- src/components/SignIn/SignOut.tsx
+++ src/components/SignIn/SignOut.tsx
@@ -1,6 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import type { FC } from "react";
-import { Button, Avatar } from "antd";
+import { Button, Avatar, Modal } from "antd";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import AuthContext from "../../store/auth-context";
@@ -10,11 +10,30 @@ import "firebaseui/dist/firebaseui.css";
 
 const SignOut: FC = () => {
   const { user } = useContext<AuthContextType>(AuthContext);
+  const [isLogoutModalVisible, setIsLogoutModalVisible] = useState<boolean>(false);
+
+  const handleOpenLogoutConfirm = () => {
+    setIsLogoutModalVisible(true);
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    setIsLogoutModalVisible(false);
+  };
 
   return (
     <div className="sign-out">
-      <Button onClick={() => firebase.auth().signOut()}>Sign-out</Button>
+      <Button onClick={handleOpenLogoutConfirm}>Sign-out</Button>
       {user && <Avatar src={user.photoURL} />}
+      <Modal
+        title="Log Out"
+        open={isLogoutModalVisible}
+        onOk={handleLogout}
+        okText="Yes, Log Out"
+        onCancel={() => setIsLogoutModalVisible(false)}
+      >
+        <p>Are you sure you want to log out?</p>
+      </Modal>
     </div>
   );
 };
