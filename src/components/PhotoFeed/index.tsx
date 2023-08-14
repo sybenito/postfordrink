41af64906da7b14f77928d93cb6b8c14e700e2a1
Ben Sy <ben.sy@ttt.studio>
2023-08-14 11:19:17 -0700

    lazy load photo feed.  Rough static implementation

--- src/components/PhotoFeed/index.tsx
+++ src/components/PhotoFeed/index.tsx
@@ -1,8 +1,38 @@
 import React from "react";
 import type { FC } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Spin } from "antd";
 
 const PhotoFeed: FC = () => {
-  return <div>PhotoFeed</div>;
+  const photos = [
+    {
+      id: 1,
+      photoUrl: "https://picsum.photos/500",
+    },
+    {
+      id: 2,
+      photoUrl: "https://picsum.photos/500",
+    },
+    {
+      id: 3,
+      photoUrl: "https://picsum.photos/500",
+    },
+  ];
+
+  const renderPhotos: () => JSX.Element[] = () =>
+    photos.map((photo) => (
+      <div className="item" key={photo.id}>
+        <img src={photo.photoUrl} alt={`Feed ${photo.id}`} />
+      </div>
+    ));
+
+  return (
+    <div className="photo-feed">
+      <InfiniteScroll dataLength={photos.length} next={() => {}} hasMore loader={<Spin />}>
+        {renderPhotos()}
+      </InfiniteScroll>
+    </div>
+  );
 };
 
 export default PhotoFeed;
