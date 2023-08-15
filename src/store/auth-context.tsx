82802c956339475cb46bd5e1698d0ef4a8ae14ac
Ben Sy <ben.sy@ttt.studio>
2023-08-15 11:53:10 -0700

    fix extra reloading in email signup registered page

--- src/store/auth-context.tsx
+++ src/store/auth-context.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { AuthContextType } from "@/hooks/Auth";
+import type { AuthContextType } from "src/hooks/Auth";
 
 const AuthContext = React.createContext<AuthContextType>({
   isSignedIn: false,
